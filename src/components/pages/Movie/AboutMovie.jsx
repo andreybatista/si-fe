e2bfdccee7e7f6dd2c6455142021a92634040027
andreybatista <andreybatista131@gmail.com>
2022-04-26 09:36:44 -0300

    home_alpha 0.2.3

--- src/components/pages/Movie/AboutMovie.jsx
+++ src/components/pages/Movie/AboutMovie.jsx
@@ -2,9 +2,20 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import OwlCarousel from "react-owl-carousel2";
 
+import Loading from "../../Loading";
+import useMembers from "../../../../hooks/query/movie/useMembers";
+
 import Members from "./members";
 
 export default function AboutMovie({ movie }) {
+  const members = useMembers(movie.id);
+
+  if (members.isLoading) {
+    return <Loading />;
+  }
+  
+  console.log(members.data.cast);
+
   return (
     <>
       <div className="aboutMovie">
@@ -13,7 +24,6 @@ export default function AboutMovie({ movie }) {
           <h2>{movie.original_title}</h2>
           <p>{movie.overview}</p>
         </Container>
-        <Members id={movie.id} />
       </div>
     </>
   );
--- src/components/pages/Movie/Members.jsx
+++ src/components/pages/Movie/Members.jsx
@@ -3,8 +3,7 @@ import OwlCarousel from "react-owl-carousel2";
 import useMembers from "../../../../hooks/query/movie/useMembers";
 import Loading from "../../Loading";
 
-export default function AboutMovie({ id }) {
-  const members = useMembers(id);
+export default function AboutMovie({ members }) {
   const options = {
     items: 2,
     dots: true,
@@ -12,24 +11,18 @@ export default function AboutMovie({ id }) {
     loop: true,
     mergeFit: false,
   };
-  if (members.isLoading) {
-    return <Loading />;
-  }
+  // if (members.isLoading) {
+  //   return <Loading />;
+  // }
   return (
     <>
       <OwlCarousel options={options}>
-        {members.data.cast.map((member) => (
-          <>
-            {member.profile_path ? (
-              <div key={member.credit_id}>
-                <img
-                  src={`https://image.tmdb.org/t/p/original/${member.profile_path}`}
-                />
-              </div>
-            ) : (
-              ""
-            )}
-          </>
+        {members.map((member) => (
+          <div key={member.credit_id}>
+            <img
+              src={`https://image.tmdb.org/t/p/original/${member.profile_path}`}
+            />
+          </div>
         ))}
       </OwlCarousel>
     </>
