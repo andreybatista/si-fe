c059fa357941e423e48ad936aef97e8fe0990943
andreybatista <andreybatista131@gmail.com>
2022-04-26 10:25:14 -0300

    home_alpha 0.2.3

--- src/components/pages/Movie/AboutMovie.jsx
+++ src/components/pages/Movie/AboutMovie.jsx
@@ -5,16 +5,15 @@ import OwlCarousel from "react-owl-carousel2";
 import Loading from "../../Loading";
 import useMembers from "../../../../hooks/query/movie/useMembers";
 
-
-export default function AboutMovie({ movie }) {
-  const members = useMembers(movie.id);
-
-  if (members.isLoading) {
-    return <Loading />;
-  }
-  
-  console.log(members.data.cast);
-
+export default function AboutMovie({ movie, members }) {
+  const options = {
+    items: 2,
+    dots: true,
+    stagePadding: 15,
+    loop: true,
+    mergeFit: false,
+  };
+ 
   return (
     <>
       <div className="aboutMovie">
@@ -23,6 +22,15 @@ export default function AboutMovie({ movie }) {
           <h2>{movie.original_title}</h2>
           <p>{movie.overview}</p>
         </Container>
+        <OwlCarousel options={options}>
+          {members.cast.map((member) => (
+            <div key={member.credit_id}>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${member.profile_path}`}
+              />
+            </div>
+          ))}
+        </OwlCarousel>
       </div>
     </>
   );
--- src/pages/movie.jsx
+++ src/pages/movie.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+import useMembers from "../../hooks/query/movie/useMembers";
 import useMovieDetails from "../../hooks/query/movie/useMovieDetails";
 
 import Loading from "../components/Loading";
@@ -10,15 +11,16 @@ import MainImage from "../components/pages/Movie/MainImage";
 function Movie() {
   const { movie_id } = useParams();
   const movieDetails = useMovieDetails(movie_id);
+  const members = useMembers(movie_id);
 
-  if (movieDetails.isLoading) {
+  if (movieDetails.isLoading || members.isLoading) {
     return <Loading />;
   }
 
   return (
     <>
       <MainImage movie={movieDetails.data} />
-      <AboutMovie movie={movieDetails.data} />
+      <AboutMovie movie={movieDetails.data} members={members.data} />
     </>
   );
 }
