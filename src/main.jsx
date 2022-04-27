f61673023eb8f1e26c191ad7cdc2dc7fb5f53eaf
andreybatista <andreybatista131@gmail.com>
2022-04-27 20:37:03 -0300

    home_alpha 0.5

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import * as ReactDOMClient from "react-dom/client";
 
 import App from "./App";
@@ -7,5 +8,10 @@ import "react-owl-carousel2/src/owl.carousel.css";
 import "react-loading-skeleton/dist/skeleton.css";
 import "react-circular-progressbar/dist/styles.css";
 
-const root = ReactDOMClient.createRoot(document.getElementById("root"));
-root.render(<App />);
+// const root = ReactDOMClient.createRoot(document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
