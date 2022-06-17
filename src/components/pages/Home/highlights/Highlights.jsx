f93484eaf81046c29e08f4f181cd62ae5c4bc63d
Andrey <andreybatista131@gmail.com>
2022-06-17 08:01:00 -0300

    fix/img

--- src/components/pages/Home/highlights/Highlights.jsx
+++ src/components/pages/Home/highlights/Highlights.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import OwlCarousel from "react-owl-carousel";
+import OwlCarousel from "react-owl-carousel2";
 
 import styles from "./highlights.module.css";
 
-import { Col, Row } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
 import ProgressBar from "./ProgressBar";
 
 export default function Highlights({ highlights }) {
@@ -11,38 +11,43 @@ export default function Highlights({ highlights }) {
   if (!highlights) {
     return "teste";
   }
+  const options = {
+    items: 1,
+    dots: false,
+    autoplay: false,
+    stagePadding: 0,
+    autoplayHoverPause: true,
+    loop: true,
+    autoplayTimeout: 5000
+  };
   return (
-    <OwlCarousel
-      className="owl-theme"
-      loop
-      items={1}
-      dots={false}
-      autoplay={true}
-      autoplayHoverPause={true}
-      autoplayTimeout={5000}
+    <OwlCarousel options={options}
     >
       {highlights.data.results?.map((highlight) => (
         <div
           key={highlight.id}
           className={styles.background}
           style={{
-            backgroundImage: `url(https://image.tmdb.org/t/p/original/${highlight.poster_path})`,
+            backgroundImage: `url(https://image.tmdb.org/t/p/original/${window.screen.width < 500 ? highlight.poster_path : highlight.backdrop_path})`,
           }}
         >
           <div className={styles.boxText}>
-            <h1>{highlight.title}</h1>
-            <p>{highlight.overview}</p>
-            <Row>
-              <Col className="d-flex flex-direction-column align-items-center">
-                <ProgressBar />
-              </Col>
-              <Col className="d-flex justify-content-end">
-                <a className={styles.btnLink}>Continue</a>
-              </Col>
-            </Row>
+            <Container fluid>
+              <h1>{highlight.title}</h1>
+              <p>{highlight.overview}</p>
+              <Row>
+                <Col className="d-flex flex-direction-column align-items-center">
+                  <ProgressBar />
+                </Col>
+                <Col className="d-flex justify-content-end">
+                  <a className={styles.btnLink}>Continue</a>
+                </Col>
+              </Row>
+            </Container>
           </div>
         </div>
-      ))}
-    </OwlCarousel>
+      ))
+      }
+    </OwlCarousel >
   );
 }
--- src/components/pages/Home/movieList/MovieList.jsx
+++ src/components/pages/Home/movieList/MovieList.jsx
@@ -3,8 +3,7 @@ import OwlCarousel from "react-owl-carousel2";
 import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
 
 import styles from "./movielist.module.css";
-import { Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function MovieList({ movies, title }) {
   if (!movies) {
@@ -16,11 +15,50 @@ export default function MovieList({ movies, title }) {
     stagePadding: 15,
     loop: true,
     mergeFit: false,
+    responsive: {
+      0: {
+        items: 2
+      },
+      500: {
+        items: 3
+      },
+      700: {
+        items: 4
+      },
+      900: {
+        items: 5
+      },
+      1400: {
+        items: 6
+      },
+      1600: {
+        items: 7
+      },
+      1900: {
+        items: 8
+      },
+      2100: {
+        items: 9
+      },
+      2400: {
+        items: 10
+      },
+      2700: {
+        items: 11
+      },
+      3000: {
+        items: 12
+      }
+    }
   };
   return (
     <div className={styles.carouselMovies + " movieSlideList"}>
-      <Container>
-        <h2 className={styles.title}>{title}</h2>
+      <Container fluid>
+        <Row>
+          <Col>
+            <h2 className={styles.title}>{title}</h2>
+          </Col>
+        </Row>
       </Container>
       <OwlCarousel options={options}>
         {movies.data.results?.map((movie) => (
@@ -29,9 +67,9 @@ export default function MovieList({ movies, title }) {
             key={movie.id}
             className={styles.boxPoster}
           >
-            <div className={styles.container}>
-              <img className="img-fluid" width={480} height={800} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}/>
-              <div className={styles.image}>
+            <div className={'position-relative ' + styles.container}>
+              <img alt={movie.title} style={{ objectFit: 'cover' }} width={'100%'} height={window.screen.width < 500 ? 250 : 350} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} />
+              <div className={styles.boxInfo}>
                 <CircularProgressbar
                   value={movie.vote_average.toFixed(1)}
                   text={`${movie.vote_average.toFixed(1)}`}
@@ -39,9 +77,7 @@ export default function MovieList({ movies, title }) {
                   className={styles.CircularProgressbar}
                   background={true}
                   styles={buildStyles({
-                    // How long animation takes to go from one percentage to another, in seconds
                     pathTransitionDuration: 0.5,
-                    // Colors
                     textSize: "30px",
                     path: {
                       stroke: `rgba(62, 152, 199, ${3 / 100})`,
